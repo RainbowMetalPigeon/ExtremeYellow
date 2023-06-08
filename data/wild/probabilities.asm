54427afb545a9be5f8f7dc52b183ec44d8e82a8e
Michael De Nuccio <michael.de.nuccio@desy.de>
2023-06-08 01:58:45 -0700

    proxy of the new encounter probabilities, but mostly a git token test

--- data/wild/probabilities.asm
+++ data/wild/probabilities.asm
@@ -13,3 +13,17 @@ WildMonEncounterSlotChances:
 	db 241, $0E ; 13/256 =  5.1% chance of slot 7
 	db 252, $10 ; 11/256 =  4.3% chance of slot 8
 	db 255, $12 ;  3/256 =  1.2% chance of slot 9
+
+;	db  50, $00 ; 51/256 = 19.9% chance of slot 0
+;	db 101, $02 ; 51/256 = 19.9% chance of slot 1
+;	db 140, $04 ; 39/256 = 15.2% chance of slot 2
+;	db 165, $06 ; 25/256 =  9.8% chance of slot 3
+;	db 190, $08 ; 25/256 =  9.8% chance of slot 4
+;	db 215, $0A ; 25/256 =  9.8% chance of slot 5
+;	db 228, $0C ; 13/256 =  5.1% chance of slot 6
+;	db 241, $0E ; 13/256 =  5.1% chance of slot 7
+;	db 249, $10 ; 11/256 =  4.3% chance of slot 8
+;	db 252, $12 ;  3/256 =  1.2% chance of slot 9
+;	db 253, $14 ;  1/256 =  0.4% chance of slot 10
+;	db 254, $16 ;  1/256 =  0.4% chance of slot 11
+;	db 255, $18 ;  1/256 =  0.4% chance of slot 12
