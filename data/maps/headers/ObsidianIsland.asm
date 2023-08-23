7b1f29a1a631ada7bfc7874efc191c4d45907316
RainbowMetalPigeon <denuccio.michael@gmail.com>
2023-08-23 01:24:02 -0700

    fixed connection between Obsidian and 28, found out there's a palette glitch in obsidian when you interact with Pikachu

--- data/maps/headers/ObsidianIsland.asm
+++ data/maps/headers/ObsidianIsland.asm
@@ -1,4 +1,4 @@
 	map_header ObsidianIsland, OBSIDIAN_ISLAND, OVERWORLD, NORTH | EAST
 	connection north, Route27, ROUTE_27, 6
-	connection east, Route28, ROUTE_28, 7
+	connection east, Route28, ROUTE_28, 4
 	end_map_header
--- data/maps/headers/Route28.asm
+++ data/maps/headers/Route28.asm
@@ -1,4 +1,4 @@
 	map_header Route28, ROUTE_28, OVERWORLD, WEST | EAST
-	connection west, ObsidianIsland, OBSIDIAN_ISLAND, -7
+	connection west, ObsidianIsland, OBSIDIAN_ISLAND, -4
 	connection east, VermilionCity, VERMILION_CITY, -7
 	end_map_header
