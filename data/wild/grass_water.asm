64f34047a64dda3978fa3b8ed4bc984fbd31cbc5
RainbowMetalPigeon <denuccio.michael@gmail.com>
2024-02-20 15:56:28 +0100

    added Vermilion Dock surfing encounters

--- data/wild/grass_water.asm
+++ data/wild/grass_water.asm
@@ -99,7 +99,7 @@ WildDataPointers:
 	dw NothingWildMons
 	dw NothingWildMons
 	dw NothingWildMons
-	dw NothingWildMons			; VermilionDockWildMons VERMILION_DOCK, wannabe
+	dw VermilionDockWildMons ; new
 	dw NothingWildMons
 	dw NothingWildMons
 	dw NothingWildMons
@@ -324,7 +324,7 @@ INCLUDE "data/wild/maps/CeruleanCave2F.asm"
 INCLUDE "data/wild/maps/CeruleanCaveB1F.asm"
 INCLUDE "data/wild/maps/PowerPlant.asm"
 INCLUDE "data/wild/maps/Route23.asm"
-;INCLUDE "data/wild/maps/VermilionDock.asm"
+INCLUDE "data/wild/maps/VermilionDock.asm"
 INCLUDE "data/wild/maps/VictoryRoad2F.asm"
 INCLUDE "data/wild/maps/VictoryRoad3F.asm"
 INCLUDE "data/wild/maps/VictoryRoad1F.asm"
