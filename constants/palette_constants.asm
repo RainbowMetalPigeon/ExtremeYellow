833cbaae40230b9d45d4ddd1eb45de2b48729cb6
RainbowMetalPigeon <denuccio.michael@gmail.com>
2024-05-24 03:52:17 +0200

    edit title screen, 2

--- constants/palette_constants.asm
+++ constants/palette_constants.asm
@@ -99,6 +99,7 @@ DEF SET_PAL_DEFAULT EQU $ff
 	const PAL_EETERNATUSSHINY ; new
 	const PAL_ARCEUSSHINY ; new
 	const PAL_MISSINGNOSHINY ; new
+	const PAL_EXTREME ; new
 DEF NUM_SGB_PALS_HAUNTEDLESS EQU const_value ; new, used only to select palettes in Haunted maps
 	const PAL_PALLET_00 ; new
 	const PAL_PALLET_01 ; new
