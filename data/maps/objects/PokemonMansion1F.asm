125bda47c3f0e4cd5e9fd0ef8edeb0681928c983
RainbowMetalPigeon <denuccio.michael@gmail.com>
2024-09-24 12:23:09 +0200

    updated teams of sea routes and cinnabar mansion

--- data/maps/objects/PokemonMansion1F.asm
+++ data/maps/objects/PokemonMansion1F.asm
@@ -14,7 +14,7 @@ PokemonMansion1F_Object:
 	def_bg_events
 
 	def_object_events
-	object_event 17, 17, SPRITE_SCIENTIST, STAY, LEFT, 1, OPP_SCIENTIST, 4
+	object_event 17, 17, SPRITE_SCIENTIST, STAY, LEFT, 1, OPP_SCIENTIST, 1 ; edited, was 4
 	object_event 14,  3, SPRITE_POKE_BALL, STAY, NONE, 2, ESCAPE_ROPE
 	object_event 18, 21, SPRITE_POKE_BALL, STAY, NONE, 3, CARBOS
 
