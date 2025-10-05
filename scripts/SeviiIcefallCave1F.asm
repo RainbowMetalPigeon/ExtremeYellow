833cf56b7ce1a60ae89c74806d4476f59dfd53da
RainbowMetalPigeon <denuccio.michael@gmail.com>
2025-10-05 15:01:40 +0200

    minor edits, including fixing a bug with Route 4 map appeared out of nowhere

--- scripts/SeviiIcefallCave1F.asm
+++ scripts/SeviiIcefallCave1F.asm
@@ -2,9 +2,9 @@ SeviiIcefallCave1F_Script:
 	jp EnableAutoTextBoxDrawing
 
 SeviiIcefallCave1F_TextPointers:
-	dw BoulderText
-	dw BoulderText
-	dw RockSmashText
+;	dw BoulderText
+;	dw BoulderText
+;	dw RockSmashText
 
 ;SeviiRoute31Text3:
 ;	text_far _SeviiRoute31Text3
--- scripts/SeviiRoute37.asm
+++ scripts/SeviiRoute37.asm
@@ -1,26 +1,15 @@
-SeviiRoute37_Object:
-	db $43 ; border block
+SeviiRoute37_Script:
+	jp EnableAutoTextBoxDrawing
 
-	def_warp_events
-;	warp_event 28, 33, SEVII_ONE_ISLAND_DOCK, 1
-;	warp_event 29, 33, SEVII_ONE_ISLAND_DOCK, 2
+SeviiRoute37_TextPointers:
+;	dw RockSmashText
+;	dw RockSmashText
+;	dw PickUpItemText
+;	dw PickUpItemText
+;	dw PickUpItemText
+;	dw SeviiRoute37Text1
+	text_end
 
-	def_bg_events
-	bg_event  4, 37, 15 ; SeviiRoute36SignText1
-
-	def_object_events
-;	object_event  5, 38, SPRITE_SUPER_NERD, STAY, UP, 1 ;
-	; trainers
-
-	object_event  9, 30, SPRITE_COOLTRAINER_F, STAY, LEFT, 6, OPP_COOLTRAINER, 82 ; 3
-	object_event  8, 35, SPRITE_COOLTRAINER_F, STAY, UP, 6, OPP_COOLTRAINER, 82 ; 4
-	object_event 10, 32, SPRITE_COOLTRAINER_F, STAY, UP, 6, OPP_COOLTRAINER, 82 ; 4
-	object_event  8, 40, SPRITE_COOLTRAINER_F, STAY, RIGHT, 6, OPP_COOLTRAINER, 82 ; 4
-
-	object_event  7, 42, SPRITE_COOLTRAINER_F, STAY, ANY_DIR, 6, OPP_COOLTRAINER, 82 ; 4
-
-	; non-persons
-;	object_event  5, 29, SPRITE_ROCKSMASHABLE_ROCK, STAY, NONE, 11
-;	object_event  2, 47, SPRITE_POKE_BALL, STAY, NONE, 14, METAL_COAT
-
-	def_warps_to SEVII_ROUTE_37
+;SeviiRoute37Text1:
+;	text_far _SeviiRoute37Text1
+;	text_end
