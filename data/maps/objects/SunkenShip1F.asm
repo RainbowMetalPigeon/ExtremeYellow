5bac16e3f4149004aa8d59cd0b0e3900ac90dda5
RainbowMetalPigeon <denuccio.michael@gmail.com>
2025-12-21 00:47:00 +0100

    fixed some bugs with Sunken Ship warps and obstacles

--- data/maps/objects/SunkenShip1F.asm
+++ data/maps/objects/SunkenShip1F.asm
@@ -12,7 +12,7 @@ SunkenShip1F_Object:
 	warp_event 33, 19, SUNKEN_SHIP_ROOMS,  7 ;  6, room 8
 	; stairs
 	warp_event 21,  5, SUNKEN_SHIP_1F,    17 ;  7
-	warp_event 34, 10, SUNKEN_SHIP_1F,    18 ;  8
+	warp_event 34, 10, SUNKEN_SHIP_1F,    19 ;  8
 	warp_event 22, 18, SUNKEN_SHIP_1F,    22 ;  9
 
 ; mid floor
@@ -26,8 +26,8 @@ SunkenShip1F_Object:
 	warp_event  0, 39, SUNKEN_SHIP_ROOMS, 26 ; 16, captain's room
 	; stairs
 	warp_event 21, 33, SUNKEN_SHIP_1F,     7 ; 17
-	warp_event 23, 33, SUNKEN_SHIP_1F,     8 ; 18
-	warp_event 34, 38, SUNKEN_SHIP_1F,    30 ; 19
+	warp_event 23, 33, SUNKEN_SHIP_1F,    30 ; 18
+	warp_event 34, 38, SUNKEN_SHIP_1F,     8 ; 19
 	warp_event  7, 39, SUNKEN_SHIP_1F,    31 ; 20
 	warp_event 34, 40, SUNKEN_SHIP_1F,    32 ; 21
 	warp_event 22, 46, SUNKEN_SHIP_1F,     9 ; 22
@@ -43,7 +43,7 @@ SunkenShip1F_Object:
 	warp_event 27, 75, SUNKEN_SHIP_ROOMS, 22 ; 28, room 7
 	warp_event 33, 75, SUNKEN_SHIP_ROOMS, 24 ; 29, room 8
 	; stairs
-	warp_event 23, 61, SUNKEN_SHIP_1F,    19 ; 30
+	warp_event 23, 61, SUNKEN_SHIP_1F,    18 ; 30
 	warp_event  7, 67, SUNKEN_SHIP_1F,    20 ; 31
 	warp_event 34, 68, SUNKEN_SHIP_1F,    21 ; 32
 
