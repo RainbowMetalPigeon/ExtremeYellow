d6b774b800959c25f06d936037f574238b9d9c8b
RainbowMetalPigeon <denuccio.michael@gmail.com>
2026-02-20 19:07:58 +0100

    fixed a bug in the type chart displayer; updated layout of TCG mode type chart

--- engine/miscellanea/menus.asm
+++ engine/miscellanea/menus.asm
@@ -17,7 +17,7 @@ DrawTypeChartInfo::
 	and a
 	jr z, .loopOffensive
 	ld de, TypeListsForChart_TCG
-	hlcoord  2,  0 ; TBE
+	hlcoord  2,  2
 	
 .loopOffensive
 	ld a, [de]
@@ -70,6 +70,7 @@ DrawTypeChartInfo::
 	jr .loopOffensive
 
 .doneWithOffensive
+	pop de
 
 	ret
 
@@ -102,24 +103,24 @@ PlaceTileDependingOnEffectiveness:
 	ret
 
 TypeListsForChart:
-	db NORMAL       ;  0
-	db FIRE         ;  1
-	db WATER        ;  2
-	db ELECTRIC     ;  3
-	db GRASS        ;  4
-	db ICE          ;  5
-	db FIGHTING     ;  6
-	db POISON       ;  7
-	db GROUND       ;  8
-	db FLYING       ;  9
-	db PSYCHIC_TYPE ; 10
-	db BUG          ; 11
-	db ROCK         ; 12
-	db GHOST        ; 13
-	db DRAGON       ; 14
-	db DARK         ; 15
-	db STEEL        ; 16
-	db FAIRY        ; 17
+	db NORMAL       ; $00
+	db FIRE         ; $14
+	db WATER        ; $15
+	db ELECTRIC     ; $17
+	db GRASS        ; $16
+	db ICE          ; $19
+	db FIGHTING     ; $01
+	db POISON       ; $03
+	db GROUND       ; $04
+	db FLYING       ; $02
+	db PSYCHIC_TYPE ; $18
+	db BUG          ; $07
+	db ROCK         ; $05
+	db GHOST        ; $08
+	db DRAGON       ; $1A
+	db DARK         ; $1B
+	db STEEL        ; $09
+	db FAIRY        ; $1C
 	db -1
 
 TypeListsForChart_TCG:
@@ -138,67 +139,109 @@ TypeListsForChart_TCG:
 DrawTypeHeader:
 	ld a, [wPersonalizationTCGMode]
 	and a
-	jr z, .nonTCG
-; TCG
-	; COLORLESS ; TBE
-	hlcoord  0,  0
-	ld [hl], $00
+	jp z, .nonTCG
+; TCG vertical
+	; COLORLESS
+	hlcoord  0,  2
+	ld [hl], $30
 	inc hl
-	ld [hl], $01
-
+	ld [hl], $31
 	; FIRE
-	hlcoord  0,  1
+	hlcoord  0,  3
 	ld [hl], $18
 	inc hl
 	ld [hl], $19
-
 	; WATER
-	hlcoord  0,  2
+	hlcoord  0,  4
 	ld [hl], $0A
 	inc hl
 	ld [hl], $0B
-
-	; LIGHTNING ; TBE
-	hlcoord  0,  3
-	ld [hl], $0C
+	; LIGHTNING
+	hlcoord  0,  5
+	ld [hl], $32
 	inc hl
-	ld [hl], $0D
-
+	ld [hl], $33
 	; GRASS
-	hlcoord  0,  4
+	hlcoord  0,  6
 	ld [hl], $1A
 	inc hl
 	ld [hl], $1B
-
 	; FIGHTING
-	hlcoord  0,  5
+	hlcoord  0,  7
 	ld [hl], $20
 	inc hl
 	ld [hl], $21
-
 	; PSYCHIC
-	hlcoord  0,  6
+	hlcoord  0,  8
 	ld [hl], $1C
 	inc hl
 	ld [hl], $1D
-
 	; DRAGON
-	hlcoord  0,  7
+	hlcoord  0,  9
 	ld [hl], $1E
 	inc hl
 	ld [hl], $1F
-
 	; DARK
-	hlcoord  0,  8
+	hlcoord  0, 10
 	ld [hl], $10
 	inc hl
 	ld [hl], $11
-
 	; STEEL
-	hlcoord  0,  9
+	hlcoord  0, 11
 	ld [hl], $08
 	inc hl
 	ld [hl], $09
+; TCG horizontal
+	; COLORLESS
+	hlcoord   2,  0
+	ld [hl], $40
+	hlcoord   2,  1
+	ld [hl], $50
+	; FIRE
+	hlcoord   3,  0
+	ld [hl], $41
+	hlcoord   3,  1
+	ld [hl], $51
+	; WATER
+	hlcoord   4,  0
+	ld [hl], $42
+	hlcoord   4,  1
+	ld [hl], $52
+	; LIGHTNING
+	hlcoord   5,  0
+	ld [hl], $43
+	hlcoord   5,  1
+	ld [hl], $53
+	; GRASS
+	hlcoord   6,  0
+	ld [hl], $44
+	hlcoord   6,  1
+	ld [hl], $54
+	; FIGHTING
+	hlcoord   7,  0
+	ld [hl], $45
+	hlcoord   7,  1
+	ld [hl], $55
+	; PSYCHIC
+	hlcoord   8,  0
+	ld [hl], $46
+	hlcoord   8,  1
+	ld [hl], $56
+	; DRAGON
+	hlcoord   9,  0
+	ld [hl], $47
+	hlcoord   9,  1
+	ld [hl], $57
+	; DARK
+	hlcoord  10,  0
+	ld [hl], $48
+	hlcoord  10,  1
+	ld [hl], $58
+	; STEEL
+	hlcoord  11,  0
+	ld [hl], $49
+	hlcoord  11,  1
+	ld [hl], $59
 
 	ret
 
