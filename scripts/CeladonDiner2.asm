ac67b0e119318207d080fba7d2067ea8fa264c2a
RainbowMetalPigeon <denuccio.michael@gmail.com>
2026-03-28 20:57:17 +0100

    minor edits

--- scripts/CeladonDiner2.asm
+++ scripts/CeladonDiner2.asm
@@ -4,25 +4,16 @@ Func_f1f31::
 	jr nz, .got_item
 	ld hl, CeladonDinerText_491a7
 	call PrintText
-;	lb bc, COIN_CASE, 1
-;	call GiveItem
-;	jr nc, .bag_full
 	SetEvent EVENT_GOT_COIN_CASE
 	ld hl, ReceivedCoinCaseText
 	call PrintText
 	call WaitForTextScrollButtonPress
 	ld hl, CoinCaseExplanationText
 	jr .printAndEnd
-;	jr .done
-;.bag_full
-;	ld hl, CoinCaseNoRoomText
-;	call PrintText
-;	jr .done
 .got_item
 	ld hl, CeladonDinerText_491b7
 .printAndEnd
 	call PrintText
-;.done
 	ret
 
 CeladonDinerText_491a7:
@@ -37,10 +28,6 @@ ReceivedCoinCaseText:
 CoinCaseExplanationText: ; new
 	text_far _CoinCaseExplanationText
 	text_end
-
-;CoinCaseNoRoomText:
-;	text_far _CoinCaseNoRoomText
-;	text_end
 
 CeladonDinerText_491b7:
 	text_far _CeladonDinerText_491b7
