49c36fa425c37ed9491aec91bf11430d220a6788
RainbowMetalPigeon <denuccio.michael@gmail.com>
2026-03-30 21:11:02 +0200

    made pallet town sign capitalization coherent

--- data/maps/objects/PalletTown.asm
+++ data/maps/objects/PalletTown.asm
@@ -40,7 +40,7 @@ PalletTown_Object:
 ;	warp_event  5,  5, SS_ANNE_1F, 2
 ;	warp_event  5,  5, SS_ANNE_1F_ROOMS, 6
 
-;	warp_event 13,  5, BLUES_HOUSE, 1
+	warp_event 13,  5, BLUES_HOUSE, 1
 ;	warp_event 13,  5, CERULEAN_CAVE_EXTRA_MIDDLE, 23
 ;	warp_event 13,  5, SECLUDED_ATOLL_SE, 1
 ;	warp_event 13,  5, SILPH_CO_9F, 1
@@ -48,12 +48,12 @@ PalletTown_Object:
 ;	warp_event 13,  5, OBSIDIAN_WAREHOUSE_FINAL, 1
 ;	warp_event 13,  5, BATTLE_FACILITY, 1
 ;	warp_event 13,  5, HAUNTED_REDS_HOUSE, 5
-	warp_event 13,  5, SECLUDED_CAVES, 4
+;	warp_event 13,  5, SECLUDED_CAVES, 4
 ;	warp_event 13,  5, SEAFOAM_ISLANDS_B4F, 3
 ;	warp_event 13,  5, VICTORY_ROAD_1F, 1
 ;	warp_event 13,  5, POKEMON_TOWER_7F, 1
 
-;	warp_event 12, 11, OAKS_LAB, 2
+	warp_event 12, 11, OAKS_LAB, 2
 ;	warp_event 12, 11, POKEMON_TOWER_6F, 1
 ;	warp_event 12, 11, HAUNTED_REDS_HOUSE, 5
 ;	warp_event 12, 11, HAUNTED_PALLET_TOWN, 1
@@ -62,7 +62,7 @@ PalletTown_Object:
 ;	warp_event 12, 11, BILLS_HOUSE, 1
 ;	warp_event 12, 11, BATTLE_FACILITY, 1
 ;	warp_event 12, 11, HALL_OF_FAME, 1
-	warp_event 12, 11, CHAMPIONS_ROOM, 1
+;	warp_event 12, 11, CHAMPIONS_ROOM, 1
 ;	warp_event 12, 11, HAUNTED_HOUSE_2, 1
 ;	warp_event 12, 11, COPYCATS_HOUSE_1F, 1
 ;	warp_event 12, 11, SECLUDED_ATOLL_SW, 1
