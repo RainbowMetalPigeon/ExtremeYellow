bfb96a968c2c5d52f06e7ad81c4a1f415f13e966
RainbowMetalPigeon <denuccio.michael@gmail.com>
2026-05-13 01:57:28 +0200

    restored warps and teams for interim V2D2

--- data/maps/objects/PalletTown.asm
+++ data/maps/objects/PalletTown.asm
@@ -2,7 +2,7 @@ PalletTown_Object:
 	db $b ; border block
 
 	def_warp_events
-;	warp_event  5,  5, REDS_HOUSE_1F, 1
+	warp_event  5,  5, REDS_HOUSE_1F, 1
 ;	warp_event  5,  5, OBSIDIAN_WAREHOUSE_FINAL, 1
 ;	warp_event  5,  5, OBSIDIAN_WAREHOUSE, 1
 ;	warp_event  5,  5, VIRIDIAN_GYM, 1
@@ -30,7 +30,7 @@ PalletTown_Object:
 ;	warp_event  5,  5, OCHRE_RESEARCH_CENTER_2, 1
 ;	warp_event  5,  5, POKEMON_TOWER_1F, 1
 ;	warp_event  5,  5, CELADON_UNIVERSITY_1, 1
-	warp_event  5,  5, HAUNTED_HOUSE_1, 1
+;	warp_event  5,  5, HAUNTED_HOUSE_1, 1
 ;	warp_event  5,  5, CELADON_HOTEL_ROOMS, 1
 ;	warp_event  5,  5, SEAFOAM_ISLANDS_B4F, 3
 ;	warp_event  5,  5, PEWTER_GYM, 1
@@ -40,7 +40,7 @@ PalletTown_Object:
 ;	warp_event  5,  5, SS_ANNE_1F, 2
 ;	warp_event  5,  5, SS_ANNE_1F_ROOMS, 6
 
-;	warp_event 13,  5, BLUES_HOUSE, 1
+	warp_event 13,  5, BLUES_HOUSE, 1
 ;	warp_event 13,  5, CERULEAN_CAVE_EXTRA_MIDDLE, 23
 ;	warp_event 13,  5, SECLUDED_ATOLL_SE, 1
 ;	warp_event 13,  5, SILPH_CO_9F, 1
@@ -52,10 +52,10 @@ PalletTown_Object:
 ;	warp_event 13,  5, SEAFOAM_ISLANDS_B4F, 3
 ;	warp_event 13,  5, VICTORY_ROAD_1F, 1
 ;	warp_event 13,  5, POKEMON_TOWER_7F, 1
-	warp_event 13,  5, MR_PSYCHICS_HOUSE, 5
+;	warp_event 13,  5, MR_PSYCHICS_HOUSE, 5
 ;	warp_event 13,  5, VIRIDIAN_GYM, 1
 
-;	warp_event 12, 11, OAKS_LAB, 2
+	warp_event 12, 11, OAKS_LAB, 2
 ;	warp_event 12, 11, POKEMON_TOWER_6F, 1
 ;	warp_event 12, 11, HAUNTED_REDS_HOUSE, 5
 ;	warp_event 12, 11, HAUNTED_PALLET_TOWN, 1
@@ -66,7 +66,7 @@ PalletTown_Object:
 ;	warp_event 12, 11, HALL_OF_FAME, 1
 ;	warp_event 12, 11, CHAMPIONS_ROOM, 1
 ;	warp_event 12, 11, HAUNTED_HOUSE_2, 1
-	warp_event 12, 11, COPYCATS_HOUSE_1F, 1
+;	warp_event 12, 11, COPYCATS_HOUSE_1F, 1
 ;	warp_event 12, 11, SECLUDED_ATOLL_SW, 1
 ;	warp_event 12, 11, CERULEAN_CAVE_EXTRA_MIDDLE, 4 ; 23 entrance, 4 exit, 7 Lance
 
